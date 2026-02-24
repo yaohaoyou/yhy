d87bfc4e6d8741e18196b339df60b93e6893d3d2
yaohaoyou <yaohaoyoutql@outlook.com>
2026-02-24 15:08:05 +0800

    modified:   winter problem/contest/2026.2.24/cat.cpp

--- winter problem/contest/2026.2.24/cat.cpp
+++ winter problem/contest/2026.2.24/cat.cpp
@@ -28,29 +28,13 @@ inline int A(int x,int y){
     for(int i=x-y+1;i<=x;i++)mmul(res,i);
     return res;
 }
-void dfs(int x,int y){
-    if(x>n){
-        bell++;
-        buc[y]++;
-        return;
-    }
-    bel[x]=y+1;
-    dfs(x+1,y+1);
-    for(int i=1;i<=y;i++){
-        bool flag=true;
-        for(int j=1;j<=x;j++)if(a[x][j]&&bel[j]==i){flag=false;break;}
-        if(!flag)   continue;
-        bel[x]=i;
-        dfs(x+1,y);
-    }
-}
-int cir(int x,int k){return x==2?immul(k,k-1):imdel(immul(k,qpow(k-1,x-1)),cir(x-1,k));}
 int main(){
     int c;
     scanf("%d%d",&c,&n);
     // printf("%d\n",cir(c,n));exit(0);
     for(int i=1;i<=n;i++)for(int j=1;j<=n;j++)scanf("%d",&a[i][j]);
     int T;scanf("%d",&T);
+    for(int i=1;i<=n;i++)if(a[i][i]){while(T--)puts("0");return 0;}
     if(c==2){
         while(T--){int k,ans;scanf("%d",&k);ans=immul(k,qpow(k-1,n-1));mmul(ans,qpow(qpow(k,n),mod-2));printf("%d\n",ans);}
         return 0;
