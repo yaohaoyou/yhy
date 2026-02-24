b9dab930a25523e8b5dba6b290cfc7f9b84f65cd
yaohaoyou <yaohaoyoutql@outlook.com>
2026-02-24 18:57:34 +0800

    2026.2.24

--- DuiPai/Normal/mine.cpp
+++ DuiPai/Normal/mine.cpp
@@ -1,86 +1,52 @@
 #include<bits/stdc++.h>
 #define ll long long
-#define int long long
 #define eb emplace_back
 #define ep emplace
 #define pii pair<int,int>
-#define pll pair<ll,ll>
-#define ppc(x) __builtin_popcountll(x)
 #define fi first
 #define se second
 #define debug(...) fprintf(stderr,__VA_ARGS__)
 #define mems(arr,x) memset(arr,x,sizeof(arr))
 #define memc(arr1,arr2) memcpy(arr1,arr2,sizeof(arr2))
 using namespace std;
-const int lgV=60;
-int n,q;
-ll L,R;
-int cnt[2];
-ll f[lgV+5][lgV+5][2];
-pll slv(ll R){
-    if(R==-1)   return pll(0,0);
-    mems(f,0);
-    f[lgV+1][0][1]=1;
-    for(int i=lgV,k=1;~i;i--,k++){
-        ll x=1ll<<i;bool o=R&x;
-        if(!o){
-            for(int j=0;j<=k;j++){
-                f[i][j][1]=f[i+1][j][1];
-                f[i][j][0]=f[i+1][j][0];
-                if(j)   f[i][j][0]+=f[i+1][j-1][0];
-            }
-        }
-        else{
-            for(int j=0;j<=k;j++){
-                if(j)   f[i][j][1]=f[i+1][j-1][1];
-                f[i][j][0]=f[i+1][j][1]+f[i+1][j][0];
-                if(j)   f[i][j][0]+=f[i+1][j-1][0];
-            }
-        }
-    }
-    pll ans=pll(0,0);
-    for(int i=0;i<=lgV;i++)if(i&1)ans.fi+=f[0][i][0]+f[0][i][1];else ans.se+=f[0][i][0]+f[0][i][1];
-    assert(ans.fi+ans.se==R+1);
-    return ans;
-}
-inline ll query(ll l,ll r){pll x=slv(r),y=slv(l-1);return max(x.fi-y.fi,x.se-y.se);}
-inline ll lowbit(ll x){return x&(-x);}
-inline ll low0(ll x){return lowbit(~x);}
+const int maxn=2e5+10;
+int n;
+int a[maxn],c[maxn];
+struct BIT{
+    int tr[maxn];
+    inline int lowbit(int x){return x&-x;}
+    inline void add(int x,int s){while(x<=n){tr[x]+=s;x+=lowbit(x);}}
+    inline int que(int x){int res=0;while(x){res+=tr[x];x^=lowbit(x);}return res;}
+    inline int query(int l,int r){return que(r)-que(l-1);}
+    void clear(){for(int i=0;i<=n;i++)tr[i]=0;}
+}T;
 void matt(int _cases){
-    scanf("%lld%lld%lld",&q,&L,&R);
-    if(!q){
-        ll x=L,l=L,r,ans=0;
-        while(l<=R){
-            L=l;x=l;r=l;
-            // if(l+low0(l)>R){ans++;l++;continue;}
-            while(L+low0(x)<=R){
-                // printf("%d,%d\n",l,x);
-                x+=low0(x);r=min(x,R);
-                l+=lowbit(l);
-            }
-            // printf("[%d,%d]\n",L,r);
-            // printf("%d %d %lld %lld %lld\n",l,l+low0(x),L,r,R);
-            ans+=query(L,r);
-            l=r+1;
-        }
-        printf("%lld\n",ans);
+    T.clear();fill(c,c+n+1,0);
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)   scanf("%d",&a[i]);
+    ll s=0,s2=0,z=0,f=0;
+    for(int i=1;i<=n;i++){
+        T.add(a[i],1);
+        s+=i-T.que(a[i]);
+        s2+=abs(i-a[i]);
+        if(a[i]-i>=0)   c[a[i]-i]++,z++;
+        else f++;
     }
-    else{
-        cnt[0]=cnt[1]=0;
-        for(ll i=L;i<=R;i++)cnt[ppc(i)&1]++;
-        if(cnt[0]>=cnt[1]){
-            for(ll i=L;i<=R;i++)putchar((!(ppc(i)&1))+'0');
-            putchar('\n');
-        }
-        else{
-            for(ll i=L;i<=R;i++)putchar((ppc(i)&1)+'0');
-            putchar('\n');
-        }
+    vector<int> ans;
+    for(int i=0;i<n;i++){
+        // printf("@ %d %d %d %d\n",z,f,s,s2);
+        if(s+s==s2) ans.eb(i);
+        s+=2*a[n-i]-n-1;
+        s2-=n-a[n-i];
+        if(a[n-i]==n)   z--,c[i]--;
+        else f--;
+        z-=c[i];f+=c[i];
+        // printf("! %d %d %d\n",z,f,c[i]);
+        s2+=-z+f;
+        s2+=abs(a[n-i]-1);
+        c[a[n-i]+i]++;z++;
     }
+    printf("%d\n",ans.size());for(int i:ans)printf("%d%c",i," \n"[i==ans.back()]);
+    if(ans.empty())puts("");
 }
-main(){int T;scanf("%lld",&T);for(int i=1;i<=T;i++)matt(i);}
-
-/*
-1000001
-1000111
-*/
+int main(){int T;scanf("%d",&T);for(int i=1;i<=T;i++)matt(i);}
