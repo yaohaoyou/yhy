543c3c0b1ee090f522c2127c0d6feb5a3ef5aba8
yaohaoyou <yaohaoyoutql@outlook.com>
2026-04-04 12:34:23 +0800

    2026.4.4

--- down/libra/grader.cpp
+++ down/libra/grader.cpp
@@ -1,12 +1,13 @@
+#include <bits/stdc++.h>
 #include "testlib.h"
-#include <iostream>
 int main(int argc, char* argv[]) {
-    registerLemonChecker(argc, argv);
+    // registerLemonChecker(argc, argv);
+    registerTestlibCmd(argc, argv);
     int out1 = ouf.readInt(), out2 = ouf.readInt(), ans1 = ans.readInt(), ans2 = ans.readInt();
     if (out1 != ans1)
         quitf(_wa, "WA");
     else if (out2 != ans2)
-    	quitp(0.4*atoi(argv[4]), "Partially Correct");
+    	quitf(_ok, "Partially Correct");
     else quitf(_ok, "Accepted");
     return 0;
 }
